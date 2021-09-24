f38eec480185d4264633a37496bdbe8ce4ba6f0e
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2021-09-24 22:11:20 +0200

    background & orbit controls

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,16 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
+import {OrbitControls} from 'https://threejs.org/examples/jsm/controls/OrbitControls.js'
+console.log(OrbitControls)
+const rad_to_deg=(rad)=>{
+  var pi = Math.PI;
+  return rad * (180/pi);
+}
+
+const deg_to_rad=(deg)=>{
+  var pi = Math.PI;
+  return deg / (180/pi);
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -8,17 +20,30 @@ document.body.appendChild( renderer.domElement );
 
 
 //cube
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry(20, 1, 20);
+const edges = new THREE.EdgesGeometry( geometry );
+const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xbbbbbb } ) );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+scene.add(cube, line);
+
+camera.position.z = 15
+camera.position.x = 15
+camera.position.y = 15
+
+
+camera.lookAt(0, 0, 0)
+//camera.rotation.x = deg_to_rad(1)
+scene.background = new THREE.Color( 0xa7faf2 );
+
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
 
 const loop=()=>{
 	requestAnimationFrame(loop)
+	controls.update();
 	renderer.render(scene, camera)
-	cube.rotation.x+=0.01
-	cube.rotation.y+=0.02
-	cube.rotation.z+=0.03
 }
 loop()
-camera.position.z = 5;
+
